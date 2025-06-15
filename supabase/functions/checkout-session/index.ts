dad791c6c475ca9a578fef7d8c8b5da7809fe675
Alicia Sykes <alicia@omg.lol>
2025-06-15 13:28:50 +0100

    Fix header

--- supabase/functions/checkout-session/index.ts
+++ supabase/functions/checkout-session/index.ts
@@ -57,7 +57,7 @@ serve(async (req: Request) => {
       headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       },
       status: 204,
     });
